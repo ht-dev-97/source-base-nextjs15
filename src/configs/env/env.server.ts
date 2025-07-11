68c6e4270b7a15b408366e9a1c57ee88a1fd41ee
Hai Tran <hai.tran@digitalfortress.dev>
2025-07-11 10:25:45 +0700

    feat: reorganize project folders

--- src/configs/env/env.server.ts
+++ src/configs/env/env.server.ts
@@ -1,3 +1,6 @@
 export const env = {
-  API_BASE_URL: process.env.API_BASE_URL || ''
+  API_BASE_URL: process.env.API_BASE_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || '',
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || '',
+  NODE_ENV: process.env.NODE_ENV || 'development'
 }
